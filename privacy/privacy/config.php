9779226f00689de55295891df046a57b598963cb
M A X <96746279+m-3-5@users.noreply.github.com>
2026-02-07 23:48:16 +0100

    Change database credentials in config.php
    Updated database connection details in config.php.

--- privacy/privacy/config.php
+++ privacy/privacy/config.php
@@ -2,9 +2,9 @@
 // config.php
 
 // 1. DATI DATABASE (Li trovi nel wp-config.php se non li ricordi)
-$host = 'localhost';
-$db   = 'm35_warroom';
-$user = 'm35_warroom_user';
+$host = 'xxxxxx';
+$db   = 'xxxxxxxx';
+$user = 'xxxxxxxx';
 $pass = 'xxxxxxxxx';
 
 // 2. LA TUA CHIAVE GEMINI (AI)
